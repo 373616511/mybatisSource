20612f39f0b1123b8e8d816c7fdf40ef43d9dd76
373616511@qq.com <373616511@qq.com>
2017-10-09 15:15:17 +0800

    Lock

---
+++ src/main/java/com/asyf/demo/io/TestImageBinary.java
@@ -0,0 +1,57 @@
+package com.asyf.demo.io;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+/**
+ * Created by Administrator on 2017/10/9.
+ * Java 将图片转二进制再将二进制转成图片
+ */
+public class TestImageBinary {
+
+    static BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+    static BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+
+    public static void main(String[] args) {
+        System.out.println(getImageBinary());
+        //将二进制转化成图片
+        base64StringToImage(getImageBinary());
+    }
+
+    private static String getImageBinary() {
+        File f = new File("E://images//test.jpg");
+        BufferedImage bi;
+        try {
+            bi = ImageIO.read(f);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(bi, "jpg", baos);  //经测试转换的图片是格式这里就什么格式，否则会失真
+            byte[] bytes = baos.toByteArray();
+            return encoder.encodeBuffer(bytes).trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将二进制转换为图片
+     *
+     * @param base64String
+     */
+    static void base64StringToImage(String base64String) {
+        try {
+            byte[] bytes1 = decoder.decodeBuffer(base64String);
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes1);
+            BufferedImage bi1 = ImageIO.read(bais);
+            File w2 = new File("e://QQ.jpg");// 可以是jpg,png,gif格式
+            ImageIO.write(bi1, "jpg", w2);// 不管输出什么格式图片，此处不需改动
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
