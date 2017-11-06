1314859adfe278388305bf9225d468f54813c8ca
373616511@qq.com <373616511@qq.com>
2017-11-06 14:53:56 +0800

    modify project name

--- src/main/java/com/asyf/demo/xpath/Test.java
+++ src/main/java/com/asyf/demo/xpath/Test.java
@@ -2,19 +2,18 @@ package com.asyf.demo.xpath;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.parsing.PropertyParser;
+import org.apache.ibatis.parsing.XNode;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -41,15 +40,36 @@ public class Test {
             // 获取根元素
             // 表达式可以更换为/*,/rss
             Node node = (Node) x.evaluate("/*", d, XPathConstants.NODE);
+            node = (Node) x.evaluate("book", node, XPathConstants.NODE);
             System.err.println(node.getNodeName() + "--------"
                     + node.getNodeValue());
             NamedNodeMap attributes = node.getAttributes();
             if (attributes != null) {
                 for (int i = 0; i < attributes.getLength(); i++) {
                     Node attribute = attributes.item(i);
-                    String value = PropertyParser.parse(attribute.getNodeValue(),null);
-                    System.err.println(value);
+                    String value = PropertyParser.parse(attribute.getNodeValue(), null);
+                    System.err.println(attribute.getNodeName() + "--" + value);
                 }
+            }
+            System.err.println("\r\n================获取子节点================");
+            NodeList childNodes = node.getChildNodes();
+            if (childNodes != null) {
+                for (int i = 0, n = childNodes.getLength(); i < n; i++) {
+                    Node node1 = childNodes.item(i);
+                    //只查看nodeType是元素的节点
+                    if (node1.getNodeType() == Node.ELEMENT_NODE) {
+                        System.err.println(node1.getNodeName() + "---" + node1.getTextContent());
+                    }
+                }
+            }
+            System.err.println("\r\n================NodeList================");
+            XPathExpression xp = x.compile("bookstore/book");
+            Object result = xp.evaluate(d, XPathConstants.NODESET);
+            NodeList nodes = (NodeList) result;
+            System.err.println("nodes.getLength():" + nodes.getLength());
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node item = nodes.item(i);
+                System.err.println(item.getNodeName() + "--" + nodes.item(i).getNodeValue());
             }
         } catch (IOException e) {
             e.printStackTrace();
