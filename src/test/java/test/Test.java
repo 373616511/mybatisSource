0df8b87fbd3e51c2468b8c8f56a9da68cc0a722d
373616511 <373616511@qq.com>
2018-02-27 16:16:50 +0800

    代码提交

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -5,13 +5,21 @@ package test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        String a = "1(3),2(2),3(3)";
+        String[] strs = a.split(",");
+        for (String s : strs) {
+            System.out.println(s.substring(0, s.indexOf("(")) + "----" + s.substring(s.indexOf("(") + 1, s.indexOf("(") + 2));
+        }
+        short s1 = 1;
+        //s1 = s1 + 1;
+        s1 += 1;
+        /*System.out.println("test");
         long start = System.currentTimeMillis();
         long sum = 0L;
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
             sum += i;
         }
         long end = System.currentTimeMillis();
-        System.out.println("程序耗时：" + (end - start) + "ms");
+        System.out.println("程序耗时：" + (end - start) + "ms");*/
     }
 }
