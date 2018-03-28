066220e83420800f0a9d59ab0a198188df6a203b
373616511 <373616511@qq.com>
2018-03-28 09:53:00 +0800

    代码提交

--- src/main/java/com/asyf/demo/jpush/DoMain.java
+++ src/main/java/com/asyf/demo/jpush/DoMain.java
@@ -5,21 +5,31 @@ import cn.jiguang.common.resp.APIConnectionException;
 import cn.jiguang.common.resp.APIRequestException;
 import cn.jpush.api.JPushClient;
 import cn.jpush.api.push.PushResult;
+import cn.jpush.api.push.model.Platform;
 import cn.jpush.api.push.model.PushPayload;
+import cn.jpush.api.push.model.audience.Audience;
+import cn.jpush.api.push.model.notification.AndroidNotification;
+import cn.jpush.api.push.model.notification.IosNotification;
+import cn.jpush.api.push.model.notification.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DoMain {
     private static final Logger LOG = LoggerFactory.getLogger(DoMain.class);
-    private static String MASTER_SECRET = "60d9654978a0111556de8734 ";
+    private static String MASTER_SECRET = "60d9654978a0111556de8734";
     private static String APP_KEY = "4e6f9f8ecd6d6940f6efe4d2";
     private static final String ALERT = "新消息";
 
     public static void main(String[] args) {
-        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, ClientConfig.getInstance());
+        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY);
 
         // For push, all you need do is to build PushPayload object.
-        PushPayload payload = buildPushObject_all_all_alert();
+        Map<String, String> extras = new HashMap<>();
+        extras.put("type", "100");
+        PushPayload payload = buildPushObject_all_all_alert("title_1", "content_msg", extras);
 
         try {
             PushResult result = jpushClient.sendPush(payload);
@@ -38,8 +48,17 @@ public class DoMain {
         }
     }
 
-    private static PushPayload buildPushObject_all_all_alert() {
-        return PushPayload.alertAll(ALERT);
+    private static PushPayload buildPushObject_all_all_alert(String title, String content, Map<String, String> extras) {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.all())// 设置接受的平台
+                .setAudience(Audience.all())// Audience设置为all，说明采用广播方式推送，所有用户都可以接收到
+                .setNotification(
+                        Notification.newBuilder()
+                                .addPlatformNotification(IosNotification.newBuilder().addExtras(extras).setAlert(content).build())
+                                .addPlatformNotification(AndroidNotification.newBuilder().addExtras(extras).setAlert(content).setTitle(title).build())
+                                .build()
+                )
+                .build();
     }
 
 }
