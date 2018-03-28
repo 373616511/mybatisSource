222211427c29bcded3b3ab9e0dfadfaea8d9b6a3
373616511 <373616511@qq.com>
2018-03-28 17:24:35 +0800

    代码提交

--- src/main/java/com/asyf/demo/jpush/DoMain.java
+++ src/main/java/com/asyf/demo/jpush/DoMain.java
@@ -22,17 +22,19 @@ public class DoMain {
     private static String MASTER_SECRET = "60d9654978a0111556de8734";
     private static String APP_KEY = "4e6f9f8ecd6d6940f6efe4d2";
     private static final String ALERT = "新消息";
+    private static final String TITLE = "标题111sssss";
 
     public static void main(String[] args) {
-        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY);
+        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, ClientConfig.getInstance());
 
         // For push, all you need do is to build PushPayload object.
         Map<String, String> extras = new HashMap<>();
         extras.put("type", "100");
-        PushPayload payload = buildPushObject_all_all_alert("title_1", "content_msg", extras);
+        // PushPayload pushPayload = buildPushObject_all_all_alert("title_1", "content_msg", extras);
+        PushPayload pushPayload = buildPushObject_android_tag_alertWithTitle();
 
         try {
-            PushResult result = jpushClient.sendPush(payload);
+            PushResult result = jpushClient.sendPush(pushPayload);
             LOG.info("Got result - " + result);
 
         } catch (APIConnectionException e) {
@@ -46,6 +48,15 @@ public class DoMain {
             LOG.info("Error Code: " + e.getErrorCode());
             LOG.info("Error Message: " + e.getErrorMessage());
         }
+    }
+
+    //平台是 Android，目标是 tag 为 "tag1" 的设备，内容是 Android 通知 ALERT，并且标题为 TITLE。
+    public static PushPayload buildPushObject_android_tag_alertWithTitle() {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.android())
+                .setAudience(Audience.tag("asyf_group"))
+                .setNotification(Notification.android(ALERT, TITLE, null))
+                .build();
     }
 
     private static PushPayload buildPushObject_all_all_alert(String title, String content, Map<String, String> extras) {
