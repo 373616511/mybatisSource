c5538dc20b01304987a8b3ec9da53f9c835d1a04
373616511@qq.com <373616511@qq.com>
2018-11-14 13:36:08 +0800

    commit

--- src/main/java/com/asyf/demo/decimal/DoMain.java
+++ src/main/java/com/asyf/demo/decimal/DoMain.java
@@ -6,10 +6,14 @@ public class DoMain {
 
     public static void main(String[] args) {
         BigDecimal bigDecimal = new BigDecimal("2.50");
-        Integer integer = new Integer(20);
+        Integer a = new Integer(1);
+        Integer b = new Integer(2);
         //integer = null;
-        System.out.println(bigDecimal.compareTo(BigDecimal.valueOf(integer)));
+        System.out.println(bigDecimal.compareTo(BigDecimal.valueOf(a)));
         int i = bigDecimal.intValue();
         System.out.println(i);
+
+        //Integer比较大小
+        System.out.println(a.compareTo(b));
     }
 }
